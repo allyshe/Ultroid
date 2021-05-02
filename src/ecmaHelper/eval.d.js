5effc9c6e486078c162fb305b0d13cfee15ec88e
Akash Pattnaik <akashjio66666@gmail.com>
2021-05-02 11:00:49 +0530

    fixes in evalJs

--- src/ecmaHelper/eval.d.js
+++ src/ecmaHelper/eval.d.js
@@ -9,18 +9,17 @@ const { exec } = require('child_process');
 const { appendFileSync, truncate } = require('fs');
 
 (async () => {
-    const bash = exec('node ./src/ecmaHelper/evalJs.run.js');
+    truncate('./src/ecmaHelper/evalJs.result.d.js', 0, function() {
+        console.log('Result File Truncated')
+        const bash = exec('node ./src/ecmaHelper/evalJs.run.js');
 
-    truncate('./src/ecmaHelper/evalJs.result.d.js', 0, function() { 
-        console.log('Result File Truncated') 
-    }); 
+        bash.stdout.on('data', (data) => {
+            console.log(data.toString());
+            appendFileSync('./src/ecmaHelper/evalJs.result.d.js', `${data.toString()}\n`, () => {});
+        });
 
-    bash.stdout.on('data', (data) => {
-        console.log(data.toString());
-        appendFileSync('./src/ecmaHelper/evalJs.result.d.js', `${data.toString()}\n`, () => {});
-    });
-
-    bash.stderr.on('data', (error) => {
-        appendFileSync('./src/ecmaHelper/evalJs.result.d.js', `${error}\n`, () => {});
+        bash.stderr.on('data', (error) => {
+            appendFileSync('./src/ecmaHelper/evalJs.result.d.js', `${error}\n`, () => {});
+        });
     });
 })();
