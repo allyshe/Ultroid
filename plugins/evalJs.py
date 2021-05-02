c2e101fede527e8a423e53a4ea79e5fc319d56d4
AkashPattanaik <akashjio66666@gmail.com>
2021-05-02 10:07:00 +0530

    Update evalJs.py

--- plugins/evalJs.py
+++ plugins/evalJs.py
@@ -1,3 +1,16 @@
+# Ultroid - UserBot
+# Copyright (C) 2020 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+"""
+✘ Commands Available -
+• `js <javaScriptCommands>`
+    Evaluate JavaScript code and upload.
+"""
+
 # Inbuilt
 import os
 import time
